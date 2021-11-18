c80899c2376313d8fa72b01845d2c96687bfd56f
islamboxer <islamboxingdag@gmail.com>
2021-11-18 18:59:47 +0300

    base file structure created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
  import './App.css';
-import HomePage from './homepage.component';
+ import HomePage from './pages/homepage/homepage.component';
 
-function App() {
-  return (
-    <div className="App">
-      <HomePage/>
-    </div>
-  );
-}
+ function App() {
+     return ( <
+         div className = "App" >
+         <
+         HomePage / >
+         <
+         /div>
+     );
+ }
 
-export default App;
+ export default App;
