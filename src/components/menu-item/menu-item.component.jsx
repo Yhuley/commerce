c9103c7115caee32226efb707eb9150ceb044bfa
Yhuley <yuriihuley@gmail.com>
2021-11-19 00:29:39 +0200

    app divided into folfrersand files

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,1 +1,16 @@
- 
+import React from "react";
+import "./menu-item.styles.scss"
+
+const MenuItem = ({ title, subtitle, imageUrl, size}) => (
+    <div 
+        className={`${size} menu-item`}
+        style={{backgroundImage: `url(${imageUrl})`}}
+    >
+        <div className="content">
+            <h1 className="title">{title}</h1>
+            <span className="subtitle">{subtitle}</span>
+        </div>
+    </div>
+)
+
+export default MenuItem
