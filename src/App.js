1a027e4ec1f3cc0b781051f547c5f3eb804e88d2
islamboxer <islamboxingdag@gmail.com>
2021-11-19 19:01:51 +0300

    last actual version added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
- import './App.css';
+import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Route, Routes } from "react-router-dom";
+
+const HatsPage = () => (<div>hats</div>)
+const Hat1 = () => (<div>hat1</div>)
+const Hat2 = () => (<div>hat2</div>)
 
  function App() {
-     return ( 
-     <div className="App">
-         <HomePage/>
-    </div>
+     return (
+        <Routes>
+            <Route index element={<HomePage/>}/>
+            <Route path="hats" element={<HatsPage/>}/>           
+        </Routes>
      );
  }
 
- export default App;
+ export default App; 
