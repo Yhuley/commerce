ca3876cf7a73fb92c284aed713cebfa4fcbb4f4b
Yhuley <yuriihuley@gmail.com>
2021-11-24 23:50:42 +0200

    exported total goods count in general state

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,21 +1,17 @@
-import React, { useState, useEffect } from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import "./cart-icon.styles.scss";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
-import cartReducer from "../../reducers/cart.reducer";
+import CartItem from "../cart-item/cart-item.component";
 
 const CartIcon = () => {
-    const [itemsCount, setItemsCount] = useState(0)
-    const { cartReducer: { cartItems }} = useSelector(state => state)
+    //const dispatch = useDispatch()
+    const { cartReducer: { totalCount }} = useSelector(state => state)
 
-    useEffect(() => {
-        setItemsCount(cartItems.reduce((acc, cartItem) => acc + cartItem.count, 0))
-    }, [cartItems])
-    
     return (
         <div className="cart-icon">
             <ShoppingIcon className="shopping-icon" />
-            <span className="item-count">{itemsCount}</span>
+            <span className="item-count">{totalCount}</span>
         </div>
     )
 }
--- src/reducers/cart.reducer.js
+++ src/reducers/cart.reducer.js
@@ -1,14 +1,15 @@
 import { ADD_ITEM_TO_CART } from "./actions"
 import { addItemToCart } from "./cart.utils"
 
-const initialState = { cartItems: [] }
+const initialState = { cartItems: [], totalCount: 0 }
 
 const cartReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_ITEM_TO_CART :
             return {
                 ...state,
-                cartItems: addItemToCart(state.cartItems, action.payload)
+                cartItems: addItemToCart(state.cartItems, action.payload),
+                totalCount: state.cartItems.reduce((acc, cartItem) => acc + cartItem.count, 0)
             }
         default :
             return state
