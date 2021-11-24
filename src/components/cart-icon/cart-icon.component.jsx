808b48eeb63c72317d89b85e4b0dea66061b4a81
Yhuley <yuriihuley@gmail.com>
2021-11-24 23:04:31 +0200

    counting goods in cart icon component added

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import "./cart-icon.styles.scss";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
+import cartReducer from "../../reducers/cart.reducer";
 
-const CartIcon = () => (
-    <div className="cart-icon">
-        <ShoppingIcon className="shopping-icon" />
-        <span className="item-count">0</span>
-    </div>
-)
+const CartIcon = () => {
+    const [itemsCount, setItemsCount] = useState(0)
+    const { cartReducer: { cartItems }} = useSelector(state => state)
+
+    useEffect(() => {
+        setItemsCount(cartItems.reduce((acc, cartItem) => acc + cartItem.count, 0))
+    }, [cartItems])
+    
+    return (
+        <div className="cart-icon">
+            <ShoppingIcon className="shopping-icon" />
+            <span className="item-count">{itemsCount}</span>
+        </div>
+    )
+}
 
 export default CartIcon
