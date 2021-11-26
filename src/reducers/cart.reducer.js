dead256f644028087914ca57b56836f4e5157bb5
islamboxer <islamboxingdag@gmail.com>
2021-11-26 09:25:33 +0100

    total count and total price added

--- src/reducers/cart.reducer.js
+++ src/reducers/cart.reducer.js
@@ -1,16 +1,17 @@
 import { ADD_ITEM_TO_CART } from "./actions"
 import { addItemToCart } from "./cart.utils"
-import { countAsync } from "./async"
 
 const initialState = { cartItems: [], totalCount: 0, totalPrice: 0 }
 
 const cartReducer = (state = initialState, action) => {
+    console.log(action)
     switch (action.type) {
         case ADD_ITEM_TO_CART :
             return {
                 ...state,
                 cartItems: addItemToCart(state.cartItems, action.payload),
-                totalCount: console.log(state.cartItems)
+                totalCount: state.cartItems.reduce((acc, prev) => acc + prev.count, 1),
+                totalPrice: state.cartItems.reduce((acc, prev) => acc + prev.count * action.payload.price, action.payload.price)
             }
         default :
             return state
--- src/reducers/cart.utils.js
+++ src/reducers/cart.utils.js
@@ -1,11 +1,11 @@
-export const addItemToCart = async (cartItems, cartItemToAdd) => {
-    const existingCartItems = await cartItems.find(
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    const existingCartItems = cartItems.find(
         cartItem => cartItem.id === cartItemToAdd.id && cartItem.name === cartItemToAdd.name
     )
 
     if (existingCartItems) {
          
-        return await cartItems.map(cartItem => 
+        return cartItems.map(cartItem => 
             cartItem.id === cartItemToAdd.id 
             ? { ...cartItem, count: cartItem.count + 1 }  
             : cartItem
