4be38b0b17ded8aac33176ad2395fc1f087f9e94
Yhuley <yuriihuley@gmail.com>
2021-12-01 21:53:24 +0200

    css structure back

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 import CustomButton from "../custom-button/custom-button.component";
