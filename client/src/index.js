a3daa0d9f655d082a71d0d607b166ee06ef12db3
islamboxer <islamboxingdag@gmail.com>
2022-01-03 14:24:44 +0100

    converting app to pwa 1.0

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,8 @@ import { Provider } from "react-redux";
 import { store, persistor } from './redux/index';
 import { PersistGate } from "redux-persist/integration/react";
 
+import * as serviceWorker from "./serviceWorkerRegistration.js";
+
 ReactDOM.render(
     <Provider store={store}>
       <BrowserRouter>
@@ -17,3 +19,5 @@ ReactDOM.render(
     </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.register()
